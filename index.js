b444411161f2e4db68c9bd9e15f6bb2423e50e10
priyanu17 <anu396priya@gmail.com>
2022-04-22 09:21:08 +0530

    nested callbacks

--- index.js
+++ index.js
@@ -10,8 +10,28 @@ const fs = require('fs');
 // fs.writeFileSync('./txt/output.txt', textOut);
 // console.log('File is written');
 
-// Asynchronous File read and write
+// Asynchronous file read and write
 fs.readFile('./txt/start.txt', 'utf-8', (err, data) => {
   console.log('data', data);
 });
 console.log('Next line of code');
+
+fs.readFile('./txt/start.txt', 'utf-8', (err, data1) => {
+  console.log('data 1 : ', data1);
+  fs.readFile(`./txt/${data1}.txt`, 'utf-8', (err, data2) => {
+    console.log('data 2 : ', data2);
+
+    fs.readFile('./txt/append.txt', 'utf-8', (err, data3) => {
+      console.log('data 3 : ', data3);
+      fs.writeFile(
+        './txt/final.txt',
+        `${data1}\n${data2}\n${data3}`,
+        'utf-8',
+        (err) => {
+          err && console.log('Any error : ' + err);
+          console.log('Your file is written');
+        }
+      );
+    });
+  });
+});
